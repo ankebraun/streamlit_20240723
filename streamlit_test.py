be5cc0a9ffd91b4c950ed66d4fee5912007f70d1
Anke Braun <anke.braun86@gmail.com>
2024-07-23 11:27:32 +0200

    sdf

---
+++ streamlit_test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Hello World!')
+st.subheader('this is our first streamlit app')
+st.write("Here's our first attempt at using data to create a table:")
+st.image('https://static.streamlit.io/examples/cat.jpg', caption='Sunset Love.', use_column_width=True)
+st.video('https://www.youtube.com/watch?v=jfKfPfyJRdk')
